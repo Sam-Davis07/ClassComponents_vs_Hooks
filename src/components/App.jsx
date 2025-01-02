9cc6c72c63f6df89ae514ec1c5f6ac454f1abb27
Sam Davis <149461242+Sam-Davis07@users.noreply.github.com>
2025-01-02 17:50:18 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+// import ClassComponent from "./ClassComponent";
+import FunctionalComponent from "./FunctionalComponent";
+
+// class App extends React.Component {
+//   render() {
+//     return <ClassComponent />;
+//   }
+// }
+
+function App() {
+  return <FunctionalComponent />;
+}
+
+export default App;
